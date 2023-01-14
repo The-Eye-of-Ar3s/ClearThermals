c74a99ac4ac0606b100280dbc7f7a99a955c9621
TheEyeOfAr3s <eherzig@protonmail.com>
2023-01-15 00:51:50 +0100

    Updated to 3.0.0 for SPT 3.4.1

--- make.ps1
+++ make.ps1
@@ -1,10 +1,10 @@
-if (Test-Path -Path C:\Games\ETM\user\mods\ClearThermals\) {
-    Remove-Item -Recurse -Force C:\Games\ETM\user\mods\ClearThermals\ | out-null
+if (Test-Path -Path C:\Games\SPT-DEV\user\mods\ClearThermals\) {
+    Remove-Item -Recurse -Force C:\Games\SPT-DEV\user\mods\ClearThermals\ | out-null
 }
-mkdir C:\Games\ETM\user\mods\ClearThermals\ | out-null
-Copy-Item -Recurse -Path .\* -Destination C:\Games\ETM\user\mods\ClearThermals\ | out-null
-Remove-Item C:\Games\ETM\user\mods\ClearThermals\make.ps1 | out-null
-Set-Location C:\Games\ETM\ | out-null
+mkdir C:\Games\SPT-DEV\user\mods\ClearThermals\ | out-null
+Copy-Item -Recurse -Path .\* -Destination C:\Games\SPT-DEV\user\mods\ClearThermals\ | out-null
+Remove-Item C:\Games\SPT-DEV\user\mods\ClearThermals\make.ps1 | out-null
+Set-Location C:\Games\SPT-DEV\ | out-null
 Start-Process powershell {./Aki.Server.exe}
 Start-Process powershell {./Aki.Launcher.exe}
 Set-Location C:\Users\TheEyeOfAr3s\Documents\coding\SPTarkov\ClearThermals\ | out-null
